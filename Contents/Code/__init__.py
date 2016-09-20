54bfb1bf55ca2127b3cfa6fb743d96ecbcab0723
michal.hajek <mikrop@centrum.cz>
2016-09-20 23:32:29 +0200

    Nastavení agenta

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,7 +1,16 @@
-import datetime, re, time, unicodedata, hashlib, urlparse, types, urllib, httplib, os
+# titulky.com
+# Subtitles service allowed by www.titulky.com
+
+import re
+
+TITULKYCOM_SEARCH_URL = 'http://www.titulky.com/?Fulltext=%s'
 
 def Start():
-    HTTP.CacheTime = CACHE_1HOUR * 4
+  HTTP.CacheTime = CACHE_1DAY
+  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
 
 class TitulkyComAgent(Agent.Movies):
-    name = 'Titulky.com'
+  name = 'Titulky.com'
+  languages = ['cz']
+  primary_provider = False
+  contributes_to = ['com.plexapp.plugins.titulkycom']
